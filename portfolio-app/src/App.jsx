d7efbe46360a92add0ca4ceacd5a9537f96c03e2
Roshni Yadav <[roshniyadav00001@gmail.com]>
2025-02-12 17:52:23 +0530

    Initial commit

--- portfolio-app/src/App.jsx
+++ portfolio-app/src/App.jsx
@@ -1,7 +1,8 @@
 
 
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Components/Home';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -9,19 +10,24 @@ import About from './Components/About';
 import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
 
+
 const App = () => {
   return (
     <Router >
       <Header />
       <Routes>
+      
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} /> 
       </Routes>
       <Footer />
       
     </Router>
+   
+
   );
 }
 
